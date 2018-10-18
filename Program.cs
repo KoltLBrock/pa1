fe70febf87611571160f751767191f376a40c8e9
KoltLBrock <klbrock1@buffs.wtamu.edu>
2018-10-18 14:26:49 -0500

    little bit of work

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace pa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter 0 to exit.");
+            int runtimeloop = 1;
+            while(runtimeloop > 0){
+                Console.Write("Enter 0 to exit:");
+                runtimeloop = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
+    
 }
